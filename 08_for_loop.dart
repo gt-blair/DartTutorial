c9f82b76e1a50b432a8a4455db5958f8237d30a8
gt_blair <108274036+gt-blair@users.noreply.github.com>
2023-12-02 15:49:54 +0300

    Update 08_for_loop.dart

--- 08_for_loop.dart
+++ 08_for_loop.dart
@@ -3,6 +3,12 @@ void main() {
 
 	// FOR Loop
 
+	// If the body of a for loop contains one statement
+	// You can ommit(optional) the curly braces
+	for (int j = 1; j <= 5; j++)
+		print(j);
+	
+
 	// WAP to find the even numbers between 1 to 10
 
 	for (int i = 1; i <= 10; i++) {
